d8b006400bbd87d9b58ce0f1f9bd069a6cd5953d
Cotoco25 <guilhermerrtostes@gmail.com>
2026-05-17 17:25:35 -0300

    fim

--- media1.py
+++ media1.py
@@ -1,6 +1,7 @@
 from turtle import *
 from time import sleep
 from random import randint
+import tkinter as tk
 
 t = Turtle()
 t.speed(0)
@@ -70,11 +71,41 @@ def drawsquarefractal(t,size,step = 50):
     t.fd(size / 1.5)
     t.lt(10)
     drawsquare(t, size) 
-    t.lt(90)
+    t.lt(120)
     drawsquarefractal(t,size-1,step-1)
 
 
 drawsquarefractal(t,200,100)
+tracer(0)
 
+def redesenhar_forma(valor_do_slider):
+    
+    size = int(valor_do_slider)
+
+    t.clear()
+
+    t.pu()
+    t.goto(0, -50)
+    t.setheading(90)
+    t.pd()
+    
+    drawsquarefractal(t,size,100)
+    
+    update()
+
+tela_principal = getcanvas().winfo_toplevel()
+
+meu_slider = tk.Scale(
+    tela_principal, 
+    from_=0,
+    to=360,
+    orient="horizontal", 
+    label="Mude o tamanho aqui", 
+    command=redesenhar_forma, 
+    length=300
+)
+
+meu_slider.pack(side="bottom")
+meu_slider.set(200)
 
 mainloop()
